bfc4adeb1655845783f289e3cc21b6efa78ba815
CiCi Pham <cici.pham@inl.gov>
2017-08-03 10:40:42 -0600

    Dynamic array formatting

--- codes/DynArray/DynArray/main.C
+++ codes/DynArray/DynArray/main.C
@@ -4,9 +4,9 @@
 int main(int argc, const char * argv[])
 {
   dynDoub vectDoub;
-  vectDoub.set(0,3.2);
-  vectDoub.set(1,4.3);
-  vectDoub.set(2,5.4);
+  vectDoub.set(0, 3.2);
+  vectDoub.set(1, 4.3);
+  vectDoub.set(2, 5.4);
   
   for (int i =0; i < vectDoub.size(); i++)
     printf("%f \n", vectDoub[i]);
@@ -14,8 +14,8 @@ int main(int argc, const char * argv[])
   dynArr<double> vectDouble;
   dynArr<int> vectInt;
   vectDouble.set(0, 3.2);
-  vectDouble.set(1,4.3);
-  vectDouble.set(2,5.4);
+  vectDouble.set(1, 4.3);
+  vectDouble.set(2, 5.4);
   
   return 0;
 }
