358ab234e8f42f02f40e1df7ab2e5631b7f17bac
CiCi Pham <cici.pham@inl.gov>
2017-08-10 10:32:33 -0600

    cleaned up main.C and makefile

--- codes/Calculator/Calculator/main.C
+++ codes/Calculator/Calculator/main.C
@@ -1,17 +1,15 @@
-#include "Calculator.h"
 #include "CalcFunc.h"
 
 int main(int argc, const char *argv[]) {
-  const char exprToParse [] = "1.5^2 + 2*3 + 4e-1";
+  const char exprToParse[] = "1.5^2 + 2*3 + 4e-1";
 
   CalcFunc c(exprToParse);
 
   double result = c.eval();
 
   for (int i = 0; i < strlen(exprToParse); i++)
-  {
     printf("%c", exprToParse[i]);
-  }
+
   printf(" = %f \n", result);
 
   return 0;
